fada03589a0cf0a98e89c8e20d28448e0e301cd4
hana demma <hana.demma@gmail.com>
2024-12-13 12:07:00 +0300

    eda for stock market

--- scripts/financialAnalysis.py
+++ scripts/financialAnalysis.py
@@ -40,8 +40,31 @@ def getSentimentAnalysisOfPublisher(news_data, target_publisher):
         sentiment_counts.plot(kind="bar", figsize=(10, 4), title=f'Sentiment Analysis of {target_publisher}',
                       xlabel='Sentiment categories', ylabel='Number of Published Articles',
                       color=[colors[category] for category in sentiment_counts.index])
+        
 
 
+
+def checkMissingValueOfHistoricalDataset(data_aapl,data_amzn,data_goog,data_meta,data_msft,data_nvda,data_tsla):
+    combined_df = pd.concat([data_aapl.isnull().sum(),
+                             data_goog.isnull().sum(),
+                             data_amzn.isnull().sum(),
+                             data_msft.isnull().sum(),
+                             data_meta.isnull().sum(),
+                             data_nvda.isnull().sum(),
+                            data_tsla.isnull().sum()],
+                            axis=0)
+    combined_df.head()
+
+def calculateDescriptiveStatisticsOfHistoricalData(data_aapl,data_amzn,data_goog,data_meta,data_msft,data_nvda,data_tsla):
+    aapl_stats = data_aapl['Close'].describe().to_frame('AAPL')
+    goog_stats = data_goog['Close'].describe().to_frame('GOOG')
+    amzn_stats = data_amzn['Close'].describe().to_frame('AMZN')
+    msft_stats = data_msft['Close'].describe().to_frame('MSFT')
+    meta_stats = data_meta['Close'].describe().to_frame('META')
+    nvda_stats = data_nvda['Close'].describe().to_frame('NVDA')
+    tsla_stats = data_tsla['Close'].describe().to_frame('TSLA')
+    combined_stats = pd.concat([aapl_stats, goog_stats,amzn_stats,msft_stats,meta_stats,nvda_stats,tsla_stats], axis=1)
+    return combined_stats
 
  
 
